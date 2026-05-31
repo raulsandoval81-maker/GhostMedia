2c0a571e638cc8dbf21d43b758cc44a5a9536d26
Raul Sandoval <raulsandoval81@gmail.com>
2026-05-30 17:44:31 -0700

    Set Ghost Loop AM PM planner

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -13,43 +13,54 @@ buildWeek();
 
 function buildWeek() {
 
-  const queue =
-    JSON.parse(
-      localStorage.getItem("ghost-queue") || "[]"
-    );
-
-  const days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
+  const weekPlan = [
+    {
+      day: "Monday",
+      am: "Wrestling History",
+      pm: "Youth Sports Parents"
+    },
+    {
+      day: "Tuesday",
+      am: "MMA Drama",
+      pm: "Coaching"
+    },
+    {
+      day: "Wednesday",
+      am: "Technique",
+      pm: "Underdog"
+    },
+    {
+      day: "Thursday",
+      am: "Humor",
+      pm: "Combat Culture"
+    },
+    {
+      day: "Friday",
+      am: "Boxing",
+      pm: "Wrestling History"
+    }
   ];
 
   plannerOutput.innerHTML = "";
 
-  days.forEach((day, index) => {
-
-    const item = queue[index];
+  weekPlan.forEach((slot) => {
 
     plannerOutput.innerHTML += `
-
       <div class="card">
 
-        <h2>${day}</h2>
+        <h2>${slot.day}</h2>
 
-        <p>
-          ${
-            item
-              ? item.title
-              : "No content scheduled"
-          }
-        </p>
+        <div class="page">
+          <strong>6:00 AM</strong>
+          <span>${slot.am}</span>
+        </div>
+
+        <div class="page">
+          <strong>6:00 PM</strong>
+          <span>${slot.pm}</span>
+        </div>
 
       </div>
-
     `;
 
   });
